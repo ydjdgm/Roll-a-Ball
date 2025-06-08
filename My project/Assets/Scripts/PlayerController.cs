22b9bb94879cca8a276456dede1a74015e5191a9
ydjdgm <tonytony060720@gmail.com>
2025-06-08 14:26:56 +0200

    .

---
+++ My project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+    void Start()
+    {
+        rb = GetComponent <Rigidbody>();
+    }
+    void OnMove(InputValue movementValue){
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+    private void FixedUpdated(){
+        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+
+        rb.AddForce(movement);
+    }
+}
